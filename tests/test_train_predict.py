16bfea242f23026e758b051496b2d6ed568b4a3f
Kolin Guo <kolguo@ucdavis.edu>
2020-06-07 19:58:37 -0700

    Add testcases for saving/loading ckpt-weights-only

--- tests/test_train_predict.py
+++ tests/test_train_predict.py
@@ -9,6 +9,8 @@ import shutil
 import glob
 import unittest
 
+from tensorflow import keras
+
 #from src.train import get_parser
 sys.path.append("/BrainSeg/src/")
 import train
@@ -31,6 +33,7 @@ class TestUNetFullModelLoading(unittest.TestCase):
         self.ckpt_dir = '/BrainSeg/tmp_checkpoints'
         self.log_dir = '/BrainSeg/tmp_tf_logs'
         self.save_dir = '/BrainSeg/tmp_outputs'
+        keras.backend.clear_session()   # For easy reset of keras state
 
     def test_UNet_Zero_Pad_1024_SCCE(self):
         """Tests UNet_Zero_Pad 1024 SCCE"""
@@ -113,6 +116,111 @@ class TestUNetFullModelLoading(unittest.TestCase):
             [ckpt_file, train_args.data_dir_AD, train_args.data_dir_control,
              '--patch-size=1024', '--batch-size=2',
              f'--save-dir={self.save_dir}',
+             '--test-svs-idx=0', '--predict-one-round'])
+        predict.predict(predict_args)
+
+    def tearDown(self):
+        """Teardown shared by all tests"""
+        rmdir(self.ckpt_dir)
+        rmdir(self.log_dir)
+        rmdir(self.save_dir)
+
+class TestUNetModelWeightsLoading(unittest.TestCase):
+    """Testing UNet model weights loading"""
+    def setUp(self):
+        """Setup shared by all tests"""
+        self.train_parser = train.get_parser()
+        self.predict_parser = predict.get_parser()
+        self.ckpt_dir = '/BrainSeg/tmp_checkpoints'
+        self.log_dir = '/BrainSeg/tmp_tf_logs'
+        self.save_dir = '/BrainSeg/tmp_outputs'
+        keras.backend.clear_session()   # For easy reset of keras state
+
+    def test_UNet_Zero_Pad_1024_SCCE(self):
+        """Tests UNet_Zero_Pad 1024 SCCE"""
+        train_args = self.train_parser.parse_args(
+            ['UNet_Zero_Pad', '--patch-size=1024', '--loss-func=SCCE',
+             '--batch-size=2', '--num-epochs=1',
+             '--steps-per-epoch=5', '--val-steps=5',
+             f'--ckpt-dir={self.ckpt_dir}', f'--log-dir={self.log_dir}',
+             '--ckpt-weights-only', '--file-suffix=unittest'])
+        train.train(train_args)
+
+        # Find the checkpoint file
+        ckpt_file = glob.glob(os.path.join(
+            glob.glob(os.path.join(self.ckpt_dir, '*'))[0], '*.index'))[0]
+
+        predict_args = self.predict_parser.parse_args(
+            [ckpt_file, train_args.data_dir_AD, train_args.data_dir_control,
+             '--patch-size=1024', '--batch-size=2',
+             f'--save-dir={self.save_dir}',
+             '--ckpt-weights-only', '--model=UNet_Zero_Pad',
+             '--test-svs-idx=0', '--predict-one-round'])
+        predict.predict(predict_args)
+
+    def test_UNet_Zero_Pad_1024_BSCCE(self):
+        """Tests UNet_Zero_Pad 1024 BSCCE"""
+        train_args = self.train_parser.parse_args(
+            ['UNet_Zero_Pad', '--patch-size=1024', '--loss-func=BSCCE',
+             '--batch-size=2', '--num-epochs=1',
+             '--steps-per-epoch=5', '--val-steps=5',
+             f'--ckpt-dir={self.ckpt_dir}', f'--log-dir={self.log_dir}',
+             '--ckpt-weights-only', '--file-suffix=unittest'])
+        train.train(train_args)
+
+        # Find the checkpoint file
+        ckpt_file = glob.glob(os.path.join(
+            glob.glob(os.path.join(self.ckpt_dir, '*'))[0], '*.index'))[0]
+
+        predict_args = self.predict_parser.parse_args(
+            [ckpt_file, train_args.data_dir_AD, train_args.data_dir_control,
+             '--patch-size=1024', '--batch-size=2',
+             f'--save-dir={self.save_dir}',
+             '--ckpt-weights-only', '--model=UNet_Zero_Pad',
+             '--test-svs-idx=0', '--predict-one-round'])
+        predict.predict(predict_args)
+
+    def test_UNet_Zero_Pad_1024_Sparse_Focal(self):
+        """Tests UNet_Zero_Pad 1024 Sparse_Focal"""
+        train_args = self.train_parser.parse_args(
+            ['UNet_Zero_Pad', '--patch-size=1024', '--loss-func=Sparse_Focal',
+             '--batch-size=2', '--num-epochs=1',
+             '--steps-per-epoch=5', '--val-steps=5',
+             f'--ckpt-dir={self.ckpt_dir}', f'--log-dir={self.log_dir}',
+             '--ckpt-weights-only', '--file-suffix=unittest'])
+        train.train(train_args)
+
+        # Find the checkpoint file
+        ckpt_file = glob.glob(os.path.join(
+            glob.glob(os.path.join(self.ckpt_dir, '*'))[0], '*.index'))[0]
+
+        predict_args = self.predict_parser.parse_args(
+            [ckpt_file, train_args.data_dir_AD, train_args.data_dir_control,
+             '--patch-size=1024', '--batch-size=2',
+             f'--save-dir={self.save_dir}',
+             '--ckpt-weights-only', '--model=UNet_Zero_Pad',
+             '--test-svs-idx=0', '--predict-one-round'])
+        predict.predict(predict_args)
+
+    def test_UNet_Zero_Pad_1024_Balanced_Sparse_Focal(self):
+        """Tests UNet_Zero_Pad 1024 Balanced_Sparse_Focal"""
+        train_args = self.train_parser.parse_args(
+            ['UNet_Zero_Pad', '--patch-size=1024', '--loss-func=Balanced_Sparse_Focal',
+             '--batch-size=2', '--num-epochs=1',
+             '--steps-per-epoch=5', '--val-steps=5',
+             f'--ckpt-dir={self.ckpt_dir}', f'--log-dir={self.log_dir}',
+             '--ckpt-weights-only', '--file-suffix=unittest'])
+        train.train(train_args)
+
+        # Find the checkpoint file
+        ckpt_file = glob.glob(os.path.join(
+            glob.glob(os.path.join(self.ckpt_dir, '*'))[0], '*.index'))[0]
+
+        predict_args = self.predict_parser.parse_args(
+            [ckpt_file, train_args.data_dir_AD, train_args.data_dir_control,
+             '--patch-size=1024', '--batch-size=2',
+             f'--save-dir={self.save_dir}',
+             '--ckpt-weights-only', '--model=UNet_Zero_Pad',
              '--test-svs-idx=0', '--predict-one-round'])
         predict.predict(predict_args)
 
