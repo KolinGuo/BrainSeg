03244b527e120b3ab7515a112e7072d6eadd2920
Kolin Guo <kolguo@ucdavis.edu>
2020-06-07 19:25:30 -0700

    Add load_whole_model() function

--- src/networks/models/models.py
+++ src/networks/models/models.py
@@ -1,6 +1,9 @@
 """Model Script Used By train.py and predict.py"""
 from tensorflow import keras
 
+from networks.metrics import SparseIoU, SparseMeanIoU, SparseConfusionMatrix
+from networks.losses import BalancedSCCE, SparseFocalLoss, BalancedSparseFocalLoss
+
 from .FCN import fcn_model
 from .UNet import unet_model_no_pad, unet_model_zero_pad
 
@@ -21,3 +24,21 @@ def get_model(model_name: str) -> keras.Model:
     if model_name == 'FCN':
         return fcn_model(classes=3, bn=True)
     raise ValueError('Unknown model')
+
+def load_whole_model(ckpt_filepath: str) -> keras.Model:
+    """Load whole model from checkpoint
+    This includes:
+        * Model's architecture/config
+        * Model's weight values
+        * Model's compilation information (if compile()) was called
+        * Optimizer and its state, if any
+    """
+    return keras.models.load_model(
+        ckpt_filepath,
+        custom_objects={
+            'BalancedSCCE': BalancedSCCE,
+            'SparseFocalLoss': SparseFocalLoss,
+            'BalancedSparseFocalLoss': BalancedSparseFocalLoss,
+            'SparseIoU': SparseIoU,
+            'SparseMeanIoU': SparseMeanIoU,
+            'SparseConfusionMatrix': SparseConfusionMatrix})
