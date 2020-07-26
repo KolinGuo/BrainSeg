aaa096e06b13086644164b24eac340afb67345eb
Kolin Guo <kolguo@ucdavis.edu>
2020-07-26 16:24:19 -0700

    Add resize_to_original method

--- src/image_helper.py
+++ src/image_helper.py
@@ -11,8 +11,10 @@ import argcomplete
 from tqdm import tqdm
 import numpy as np
 from PIL import Image
+import pyvips as Vips
 
 from utils.numpy_pil_helper import numpy_to_pil_binary, numpy_to_pil_palette
+from networks.dataset import save_predicted_masks
 Image.MAX_IMAGE_PIXELS = None
 
 def grayscale_to_binary(args: argparse.Namespace) -> None:
@@ -139,6 +141,71 @@ def combine_truth_binary(args: argparse.Namespace) -> None:
 
     print('Done!')
 
+def resize_to_original(args: argparse.Namespace) -> None:
+    """Resize the image to original size"""
+    # Resolve path from os.getcwd()
+    args.input_dir = os.path.abspath(args.input_dir)
+    args.ref_dir = [os.path.abspath(p) for p in args.ref_dir]
+
+    # Get all img_paths in ref_dir
+    svs_paths = sorted([p for d in args.ref_dir
+                        for p in glob.glob(os.path.join(d, '*AB*.svs'))])
+
+    # Output directory is input_dir appended with _resized
+    output_dir = args.input_dir + "_resized"
+
+    # Make output directory if not exists
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    print(f'Found {len(svs_paths)} reference images. Saving to {output_dir}')
+
+    for svs_path in tqdm(svs_paths):
+        svs_name = svs_path.split('/')[-1].replace('.svs', '')
+
+        # Get the mask image in input_dir whose name matches svs_name
+        img_path = glob.glob(os.path.join(args.input_dir, svs_name+'.png'))[0]
+
+        # Get reference width and height
+        vips_img = Vips.Image.new_from_file(svs_path, level=0)
+        width, height = vips_img.width, vips_img.height
+
+        # Open the mask image
+        img = Image.open(img_path)
+
+        # If image is in RGB mode, convert it to P mode
+        if img.mode == 'RGB':
+            img_arr = np.array(img)
+
+            # For label 0, leave as black color
+            new_img_arr = np.zeros(img_arr.shape[:-1], dtype=np.uint8)
+            # For label 1, set to cyan color: R0G255B255
+            new_img_arr = np.where(np.all(img_arr == (0, 255, 255), axis=-1), 1, new_img_arr)
+            # For label 2, set to yellow color: R255G255B0
+            new_img_arr = np.where(np.all(img_arr == (255, 255, 0), axis=-1), 2, new_img_arr)
+
+            # Tests results using assert
+            np.testing.assert_array_equal(
+                np.all(img_arr == (0, 0, 0), axis=-1), new_img_arr == 0)
+            np.testing.assert_array_equal(
+                np.all(img_arr == (0, 255, 255), axis=-1), new_img_arr == 1)
+            np.testing.assert_array_equal(
+                np.all(img_arr == (255, 255, 0), axis=-1), new_img_arr == 2)
+
+            img = numpy_to_pil_palette(new_img_arr)
+            img.putpalette([0, 0, 0, 0, 255, 255, 255, 255, 0])
+
+        print(f"{svs_name}: ({img.width}, {img.height}) => ({width}, {height})")
+        assert img.mode == 'P', f"Wrong PIL image mode, expected P get {img.mode}"
+
+        # Resize the image
+        img = img.resize((width, height))
+        img_arr = np.array(img)
+
+        save_predicted_masks(img_arr, output_dir, svs_name)
+
+    print('Done!')
+
 class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
     """For removing the extra line below subcommands in argparse"""
     def _format_action(self, action):
@@ -192,6 +259,22 @@ def get_parser():
         'input_dir', type=str,
         help='Input directory of PNG binary groundtruth images (e.g. "./groundtruth")')
     p_combinebinary.set_defaults(func=combine_truth_binary)
+
+    p_resizeoriginal = subparsers.add_parser(
+        'resize_to_original',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""Resize the image to a reference size.
+        Takes an input_dir and a ref_dir as inputs.
+        Resize images in input_dir to match the size of those SVS images in ref_dir.
+        Images are matched by their names.""",
+        help='Resize the image to a reference size')
+    p_resizeoriginal.add_argument(
+        'input_dir', type=str,
+        help='Input directory of PNG images (e.g. "./data/outputs")')
+    p_resizeoriginal.add_argument(
+        'ref_dir', type=str, nargs='+',
+        help='Reference directories of SVS images(e.g. "./data/box_Ab ./data/box_control")')
+    p_resizeoriginal.set_defaults(func=resize_to_original)
 
     return main_parser
 
